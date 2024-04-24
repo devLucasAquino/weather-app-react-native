f6fb82499c49972698ad87f2d4ad6b5facf528d6
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-04-23 22:04:15 -0300

    testing

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Hello,  first app!</Text>
       <Text></Text>
+      <Text></Text>
+      <Text>developed by Lucas</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#1c1c1c',
+    fontSize: 60
+  }
 });
